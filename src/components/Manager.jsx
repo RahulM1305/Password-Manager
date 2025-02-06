fb5f7ce6ab4b36f069cc647f8bf9236d9dba64f7
Rahul Mishra <rahulamishra1305@gmail.com>
2025-02-07 01:17:22 +0530

    creating ui

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,18 +1,93 @@
 import React from "react";
+import { useRef, useState } from "react";
 
 const Manager = () => {
+  const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+
+  const showPassword = () => {
+    alert("Show the password");
+    if (ref.current.src.includes("icons/eyecross.png")) {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/eyecross.png";
+    }
+  };
+  const savePassword = () => {
+    console.log(form);
+  };
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
         <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
       </div>
-      <div className="container mx-auto bg-slate-800">
-        <div className=" text-white flex flex-col p-4">
-          <input type="text" name="" id="" />
-          <div className="flex">
-            <input type="text" name="" id="" />
-            <input type="text" name="" id="" />
+      <div className=" mycontainer ">
+        <h1 className="text-4xl text font-bold text-center">
+          <span className="text-green-500">&lt;</span>
+          Pass
+          <span className="text-green-500">OP&gt;</span>
+        </h1>
+        <p className="text-green-900 text-lg text-center">Your own password</p>
+
+        <div className="flex flex-col p-4 text-black gap-8 items-center">
+          <input
+            value={form.site}
+            onChange={handleChange}
+            placeholder="Enter website URL"
+            className="rounded-full border border-green-500 w-full p-4 py-1"
+            type="text"
+            name="site"
+            id=""
+          />
+          <div className="flex w-full justify-between gap-8 ">
+            <input
+              value={form.username}
+              onChange={handleChange}
+              placeholder="Enter Username"
+              className="rounded-full border border-green-500 w-full p-4 py-1 "
+              type="text"
+              name="username"
+              id=""
+            />
+            <div className="relative">
+              <input
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Enter Password"
+                className="rounded-full border border-green-500 w-full p-4 py-1"
+                type="text"
+                name="password"
+                id=""
+              />
+              <span
+                className="absolute right-[3px] top-[4px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  width={26}
+                  src="/icons/eye.png"
+                  alt=""
+                />
+              </span>
+            </div>
           </div>
+          <button
+            onClick={savePassword}
+            className="flex justify-center gap-2 hover:bg-green-300 items-center bg-green-400 rounded-full px-8 py-2 w-fit border border-green-900"
+          >
+            <lord-icon
+              src="https://cdn.lordicon.com/jgnvfzqg.json"
+              trigger="hover"
+            ></lord-icon>
+            Add Password
+          </button>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-purple-200 flex justify-between items-center px-4 h-14 ">
-      <div className="logo font-bold">PassOP</div>
-      <ul>
-        <li className="flex gap-4">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">COntact</a>
-        </li>
-      </ul>
+    <nav className="bg-slate-800 text-white">
+      <div className="mycontainer flex justify-between items-center px-4 py-5 h-14 ">
+        <div className="logo font-bold text-white text-2xl">
+          <span className="text-green-500">&lt;</span>
+          Pass
+          <span className="text-green-500">OP&gt;</span>
+        </div>
+        <ul>
+          <li className="flex gap-4">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
